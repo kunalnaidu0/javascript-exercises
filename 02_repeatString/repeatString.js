07c24001d0d017b43098ebc4d7f073388cbfab72
kunalnaidu0 <kunalnaidu@live.ca>
2023-07-12 20:46:55 -0600

    completes excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // if num is less than zero return error
+    if (num < 0){
+        return 'ERROR';
+    } else {
+        // create empty string and concatenate to string for desired number
+        let finalString = "";
+        for (let i = 0; i < num ; i++){
+            finalString += string;
+        }
+        return finalString;
+    }
 };
 
 // Do not edit below this line
