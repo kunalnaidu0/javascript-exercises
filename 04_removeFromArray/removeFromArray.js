706d914dd1b36cce2092d8ec9ad5ffb9e443cd8a
kunalnaidu0 <kunalnaidu@live.ca>
2023-07-12 21:52:26 -0600

    completes exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToRemove) {
 
+    for (let i = 1; i < arguments.length; i++){
+        // loop removing value till value is not contained
+        let value = arguments[i];
+        for (let index = arrayToRemove.indexOf(value); index > -1; index = arrayToRemove.indexOf(value)){
+            arrayToRemove.splice(index, 1);
+        }
+    }
+    return arrayToRemove;
 };
 
 // Do not edit below this line
