cf5eeb5b1820e7ec9f72cea53ffc4f781cc6dbe8
kunalnaidu0 <kunalnaidu@live.ca>
2023-07-12 21:11:26 -0600

    completes exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    // reversly index string and concatonate to reverse
+    for (let i = string.length - 1; i >= 0; i--){
+        reverse += string[i];
+    }
+    return reverse;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
