5316988583b97b18890c95c789aaedbb3e6e73a6
kunalnaidu0 <kunalnaidu@live.ca>
2023-07-12 22:18:20 -0600

    completes exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof(start) != "number" || typeof(end) != "number"){
+        return "ERROR";
+    }
+    if (start < 0 || end < 0){
+        return "ERROR"; 
+    }
 
+    let lower = Math.min(start, end);
+    let upper = Math.max(start, end);
+
+    let sum = 0;
+    for (let i = lower; i < upper+1; i++){
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
